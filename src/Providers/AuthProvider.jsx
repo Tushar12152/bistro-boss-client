00cef7d4e931de648df9954521c5bbf3b817361a
Tushar12152 <tusharimran470@gmail.com>
2023-11-13 12:07:14 +0600

    useAuth hook added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,12 +1,30 @@
-import { onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useState, } from "react";
 import auth from "../FireBase/Firebase.config";
+
 
  export const AuthContext=createContext(null)
 
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
     const [loading,setLoading]=useState(true)
+
+    const createUser=(email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+
+
+    const signIn=(email,password)=>{
+        setLoading(true)
+          return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const logOut=()=>{
+         setLoading(true)
+         return signOut(auth)
+    }
 
 
     useEffect(()=>{
@@ -25,6 +43,13 @@ const AuthProvider = ({children}) => {
     const authInfo={
         user,
         loading,
+        createUser,
+        signIn,
+        logOut,
+        
+
+
+
 
 
     }
